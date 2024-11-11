9d6afb55adf79bec66a15476f062884b843395e4
bke82024 <bke82024@gmail.com>
2024-11-11 10:42:07 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//define variables from HTML
+const addButton = document.querySelector(".add-item");
+const clearButton = document.querySelector(".clear-list");
+const inputField = document.querySelector("#input-field");
+const listHeader = document.querySelector("#list-header");
+//define main variable
+const toDoList = [];
