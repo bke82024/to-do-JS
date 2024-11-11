75b4b254bfaf7b48930ce4896b62448291ee4dcd
bke82024 <bke82024@gmail.com>
2024-11-11 12:12:48 +0100

    added event listeners

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //define variables from HTML
 const addButton = document.querySelector(".add-item");
 const clearButton = document.querySelector(".clear-list");
-const inputField = document.querySelector("#input-field");
-const listHeader = document.querySelector("#list-header");
+const inputField = document.getElementById("input-field");
+const listHeader = document.getElementById("list-header");
 //define main variable
 const toDoList = [];
+
+//data from html field to array
+const fromFieldToArray = () => {
+  fieldValue = inputField.value;
+  toDoList.push(fieldValue);
+};
+fromFieldToArray();
+addButton.addEventListener("click", () => {
+  if (inputField.value !== "") {
+    fromFieldToArray();
+  }
+});
+console.log(toDoList);
+inputField.addEventListener(
+  "keydown",
+  (key = () => {
+    if (key.key === "Enter") {
+      fromFieldToArray;
+    }
+  })
+);
